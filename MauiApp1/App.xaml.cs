d9463a6f41fc6df0a41fb08812787a9de58214dc
Syed Hoque <syed.h@retailinsight.io>
2025-06-25 16:55:58 +0100

    Add initial project structure and essential files for MauiApp1

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp1;
+
+public partial class App
+{
+    private readonly AppShell _appShell;
+
+    public App(AppShell appShell)
+    {
+        _appShell = appShell;
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = new Window(_appShell);
+
+        window.Created += (_, _) => { System.Diagnostics.Trace.WriteLine("[TRACE]: Window created"); };
+
+        window.Deactivated += (_, _) => { System.Diagnostics.Trace.WriteLine("[TRACE]: Window deactivated"); };
+        window.Stopped += (_, _) => { System.Diagnostics.Trace.WriteLine("[TRACE]: Window stopped"); };
+
+        return window;
+    }
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        builder.Services.TryAddSingleton<AppShell>();
+        builder.ConfigureLifecycleEvents(ConfigureLifeCycle);
+
+        return builder.Build();
+    }
+
+    static void ConfigureLifeCycle(ILifecycleBuilder lcb)
+    {
+        lcb.AddAndroid(android => android.OnActivityResult((activity, _, _, _) =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnActivityResult is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnNewIntent((activity, _) =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnNewIntent is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnStart(activity =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnStart is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnResume(activity =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnResume is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnPostCreate((activity, _) =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnPostCreate is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnCreate((activity, _) =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnCreate is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnPause(activity =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnPause is called, with activity: " +
+                                                   activity?.Class?.Name);
+            })
+            .OnDestroy(activity =>
+            {
+                System.Diagnostics.Trace.WriteLine("[TRACE]: OnDestroy is called, with activity: " +
+                                                   activity?.Class?.Name);
+            }));
+    }
+}
